b1a579ab9658b69d8140ba7f5b01a37f71dcdaee
Martin Kraft <martin@upspin.org>
2021-03-28 16:31:32 -0400

    Created working prototype.

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+type Translation = {
+	id: string;
+	translation: string;
+};
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "showtranslationinstances" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('showtranslationinstances.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ShowTranslationInstances!');
+	let disposable = vscode.commands.registerCommand('showtranslationinstances.helloWorld', async () => {
+		let { activeTextEditor } = vscode.window;
+		let selection: string = activeTextEditor?.document.getText(activeTextEditor?.selection) || "";
+		if (selection.length == 0) {
+			console.debug('Using clipboard');
+			let onreadclipboard = (value: string) => {
+				if (value.length == 0) {
+					vscode.window.showErrorMessage('No text selected or found in clipboard');
+				}
+				selection = value;
+			};
+			let onrejectedclipboardread = (reason:any) => {
+				vscode.window.showErrorMessage('Error reading clipboard');
+			};
+			await vscode.env.clipboard.readText().then(onreadclipboard, onrejectedclipboardread);
+		}
+		const relativePattern = new vscode.RelativePattern(vscode.workspace.workspaceFolders?.[0] || "", 'i18n/en.json');
+		let uri = vscode.Uri.file(`${relativePattern.base}/${relativePattern.pattern}`);
+		let onfulfilled = (value: vscode.TextDocument) => {
+			let translations: Array<Translation> = JSON.parse(value.getText());
+			let translation = translations.find(item => item.translation == selection);
+			if (typeof translation === 'undefined' || translation === null) {
+				vscode.window.showErrorMessage(`No matching translation entry.`);
+				return;
+			}
+			vscode.commands.executeCommand('workbench.action.findInFiles', {
+				query: translation?.id,
+				filesToInclude: '*.go',
+				triggerSearch: true
+			});
+		};
+		let onrejected = (reason: any) => {
+			vscode.window.showErrorMessage('No matching translation entry');
+		};
+		vscode.workspace.openTextDocument(uri).then(onfulfilled, onrejected);
 	});
-
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
